022e55d05540a3743d4ec5553cbd43b469077a19
Patrick Pang <uncleleft@gmail.com>
2015-07-09 06:44:30 +0000

    Added fields for extracting from BHC XML.

---
+++ extract/migrations/0006_auto_20150709_1358.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('extract', '0005_auto_20150427_2018'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category3',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, verbose_name='Category 3 Name')),
+                ('article', models.ForeignKey(to='extract.Article')),
+            ],
+            options={
+                'verbose_name_plural': 'Category3',
+                'verbose_name': 'Category3',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Category35',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, verbose_name='Category 35 Name')),
+                ('article', models.ForeignKey(to='extract.Article')),
+            ],
+            options={
+                'verbose_name_plural': 'Category35',
+                'verbose_name': 'Category35',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Keyword',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, verbose_name='Keyword')),
+                ('article', models.ForeignKey(to='extract.Article')),
+            ],
+            options={
+                'verbose_name_plural': 'Keywords',
+                'verbose_name': 'Keyword',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='cat2',
+            field=models.CharField(max_length=200, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='provider',
+            field=models.CharField(max_length=200, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='unique_key',
+            field=models.CharField(max_length=200, default=None, null=True, unique=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
--- extract/models.py
+++ extract/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Article(models.Model):
     source = models.CharField(max_length=5)
     url = models.URLField(null=True, default=None, blank=True)
+    unique_key = models.CharField(max_length=200, unique=True, null=True, blank=True, default=None)
     title = models.CharField(max_length=200)
     summary = models.TextField()
     content = models.TextField()
     category = models.CharField(max_length=200, blank=True)
+    cat2 = models.CharField(max_length=200, blank=True)
+    provider = models.CharField(max_length=200, blank=True)
     remarks = models.CharField(max_length=150, null=True, blank=True)
     last_modified = models.DateTimeField(null=True, blank=True)
 
@@ -44,3 +47,39 @@ class OutLink(models.Model):
 
     def __str__(self):
         return str(self.pk)
+
+
+class Category3(models.Model):
+    article = models.ForeignKey(Article)
+    name = models.CharField("Category 3 Name", max_length=200)
+
+    class Meta:
+        verbose_name = "Category3"
+        verbose_name_plural = "Category3"
+
+    def __str__(self):
+        return self.name
+
+
+class Category35(models.Model):
+    article = models.ForeignKey(Article)
+    name = models.CharField("Category 35 Name", max_length=200)
+
+    class Meta:
+        verbose_name = "Category35"
+        verbose_name_plural = "Category35"
+
+    def __str__(self):
+        return self.name
+
+
+class Keyword(models.Model):
+    article = models.ForeignKey(Article)
+    name = models.CharField("Keyword", max_length=200)
+
+    class Meta:
+        verbose_name = "Keyword"
+        verbose_name_plural = "Keywords"
+
+    def __str__(self):
+        return self.name
